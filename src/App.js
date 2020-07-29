b216aa967e8939f3b62212bc1d32b19551e35011
S3ak <nsineke@gmail.com>
2020-07-29 16:14:59 +0200

    Scaffold collect page HTML

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Header = () => (
+  <div>
+    <h1>🏠 Collect order</h1> <div>helper</div>
+  </div>
+);
+
+const Timer = () => (
+  <div>
+    <div>08:05</div>
+    <p>minutes remaining</p>
+    <div> - </div>
+  </div>
+);
+
+const List = () => (
+  <article>
+    <div>
+      <h2>✅ Collect order #093</h2>
+      <p>Pick up instructions</p>
+      <p>Go to counter 1 on the left where the sign for ‘all pickups’ is</p>
+      <hr />
+    </div>
+
+    <div>
+      <h3>BigMac Meal</h3>
+
+      <p>Large Fanta, Super Size Fries</p>
+      <hr />
+    </div>
+  </article>
+);
+
+const CollectLocation = () => (
+  <div>
+    <h2>McDonald’s Rondebosch</h2>
+    <p>51 Main Rd, Rondebosch, Cape Town, 7700</p>
+    <div>
+      <button>⤴ Directions</button>
+      <button>📞 Call</button>
+    </div>
+  </div>
+);
+
+const CTASection = () => (
+  <div>
+    <button>Ready to deliver</button>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+      <Timer />
+      <List />
+
+      <CollectLocation />
+      <CTASection />
     </div>
   );
 }
