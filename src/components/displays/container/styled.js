4cae8015e15a42171be28c664f35885d5771e5df
s3ak <nsineke@gmail.com>
2020-07-30 17:05:38 +0200

    feat(container): create container component
    Used for layout

--- src/components/displays/container/styled.js
+++ src/components/displays/container/styled.js
@@ -4,5 +4,5 @@ import ds from "../../../utils/styles/design-system";
 export const Wrapper = styled.div`
   margin: 0 auto;
   width: 100%;
-  max-width: ${ds.layout.maxWidth};
+  max-width: ${ds.get("layout.maxWidth")};
 `;
--- src/components/displays/layout/index.js
+++ src/components/displays/layout/index.js
@@ -1,42 +1,30 @@
 import React from "react";
 import PT from "prop-types";
 import { Wrapper, HeaderSection, BodySection } from "./styled";
-
-import Skeleton from "./Skeleton";
+import Container from "../container";
 
 /**
  * The `BaseLayout` Component is used to structure the layout of the web page.
  * It utilises CSS grid to position content.
  * @author Monde Sineke <monde@gmail.com>
  */
-export const BaseLayout = ({ children, isLoading, isError, headerContent }) => {
-  if (isLoading) {
-    return <Skeleton />;
-  }
-
-  if (isError) {
-    return <div>Something went wrong</div>;
-  }
-
+export const BaseLayout = ({ children, headerContent }) => {
   return (
     <Wrapper>
-      <HeaderSection>{headerContent}</HeaderSection>
-      <BodySection>{children}</BodySection>
+      <Container>
+        <HeaderSection>{headerContent}</HeaderSection>
+        <BodySection>{children}</BodySection>
+      </Container>
     </Wrapper>
   );
 };
 
 BaseLayout.propTypes = {
   children: PT.node,
-  isLoading: PT.bool,
-  isError: PT.bool,
   /** Default header is used here */
   headerContent: PT.node,
 };
 
-BaseLayout.defaultProps = {
-  isLoading: true,
-  isError: false,
-};
+BaseLayout.defaultProps = {};
 
 export default BaseLayout;
