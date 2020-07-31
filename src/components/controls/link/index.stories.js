ae74e6db1438ef739f77e060b28fad7ef1351f2a
s3ak <nsineke@gmail.com>
2020-07-31 11:07:31 +0200

    Init link component

---
+++ src/components/controls/link/index.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { Link } from "./index";
+
+export default {
+  title: "Controls/Link",
+  component: Link,
+};
+
+export const Base = () => <Link>buy now</Link>;
