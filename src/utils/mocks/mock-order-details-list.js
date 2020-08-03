2e80b24ee39d8b7563ae4f75f21b2fe9c00d38c0
s3ak <nsineke@gmail.com>
2020-08-03 02:34:42 +0200

    test(mock order details list): create mock data

---
+++ src/utils/mocks/mock-order-details-list.js
@@ -0,0 +1,32 @@
+const mockOrderDetailsLIst = [
+  {
+    id: "1",
+    title: "BigMac Meal",
+    description: "Large Fanta, Super Size Fries",
+  },
+  {
+    id: "2",
+    title: "McChicken Meal",
+    description: "Small Coke, Medium Fries",
+  },
+  {
+    id: "3",
+    title: "Oreo McFlurry",
+  },
+  {
+    id: "4",
+    title: "McChicken Meal",
+    description: "Small Coke, Medium Fries",
+  },
+  {
+    id: "5",
+    title: "Oreo McFlurry",
+  },
+  {
+    id: "6",
+    title: "McChicken Meal",
+    description: "Small Coke, Medium Fries",
+  },
+];
+
+export default mockOrderDetailsLIst;
