18b4cddec5ddba2aba983693ddb94c0d0eb3d40c
Monde Sineke <nsineke@gmail.com>
2020-08-03 23:59:26 +0200

    New delivery page (#9)
    * chore(package.json): add countdown, use and longpressable modules
    
    Added modules for delivery intent screen
    
    * Update button component
    
    * Creae cound down indicator comp
    
    * Update stories
    
    * Update scene template
    
    * Create incoming delivery intent screen
    
    * Update global styles

---
+++ src/components/scenes/incoming-delivery-intent/index.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import PT from "prop-types";
+import { FaCheck, FaTimes } from "react-icons/fa";
+import LongPressable from "react-longpressable";
+
+import CountDownIndicator from "../../displays/count-down-indicator";
+import Button from "../../controls/button";
+
+import {
+  Wrapper,
+  HeadingSection,
+  Title,
+  DescriptionSection,
+  FooterSection,
+  ButtonGroup,
+  ControlDescription,
+  ButtonContainer,
+  Legend,
+} from "./styled";
+
+import Skeleton from "./Skeleton";
+
+/**
+ * The `IncomingDeliveryIntent` Component asks the driver to accept a new delivery.
+ * @author Monde Sineke <monde@gmail.com>
+ */
+export const IncomingDeliveryIntent = ({
+  isLoading,
+  isError,
+  errorMsg = "Something went wrong",
+  title = "New delivery",
+}) => {
+  const [isAccepted, setIsAccepted] = useState(false);
+  const [isDeclined, setIsDeclined] = useState(false);
+
+  const handleOnIntentAction = (decision) => {
+    if (isAccepted || isDeclined) {
+      return;
+    }
+
+    if (decision === "decline") {
+      setIsDeclined(true);
+    }
+
+    if (decision === "accept") {
+      setIsAccepted(true);
+    }
+  };
+
+  if (isLoading) {
+    return <Skeleton />;
+  }
+
+  if (isError) {
+    return <div>{errorMsg}</div>;
+  }
+
+  const conditionalTitle = isAccepted
+    ? "Let's go!"
+    : isDeclined
+    ? "Next time chief"
+    : title;
+
+  const conditionalDescription = isAccepted
+    ? "Loading your delivery"
+    : isDeclined
+    ? ""
+    : "Press and hold for a moment to accept";
+
+  return (
+    <Wrapper>
+      <HeadingSection>
+        <Title>{conditionalTitle}</Title>
+      </HeadingSection>
+
+      <DescriptionSection>
+        <CountDownIndicator isSuccess={isAccepted} isFailure={isDeclined} />
+      </DescriptionSection>
+
+      <FooterSection>
+        {!isAccepted && !isDeclined && (
+          <ButtonGroup>
+            <ButtonContainer>
+              <LongPressable
+                onLongPress={() => handleOnIntentAction("decline")}
+                longPressTime={700}
+              >
+                <Button onClick={() => null} kind="circle" status="muted">
+                  <FaTimes />
+                </Button>
+              </LongPressable>
+
+              <Legend>Decline</Legend>
+            </ButtonContainer>
+
+            <ButtonContainer>
+              <LongPressable
+                onLongPress={() => handleOnIntentAction("accept")}
+                longPressTime={700}
+              >
+                <Button onClick={() => null} kind="circle" status="success">
+                  <FaCheck />
+                </Button>
+              </LongPressable>
+
+              <Legend>Accept</Legend>
+            </ButtonContainer>
+          </ButtonGroup>
+        )}
+
+        <ControlDescription>{conditionalDescription}</ControlDescription>
+      </FooterSection>
+    </Wrapper>
+  );
+};
+
+IncomingDeliveryIntent.propTypes = {
+  /** This flag determines if the user see's the shimmering blank shapes animation effect
+   * you see on facebook. It relies on the skeleton subcomponent.
+   */
+  isLoading: PT.bool,
+  isError: PT.bool,
+  /** The message should originate from the api response.
+   * ErrorBoundary parent component will catch application bugs */
+  errorMsg: PT.string,
+  /** You can connect this component to global state via props */
+  state: PT.string,
+  title: PT.string,
+};
+
+export default IncomingDeliveryIntent;
---
+++ src/components/scenes/index.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default {
+  title: "Scenes",
+};
+
+export const ReadMe = () => (
+  <p>Scenes are th same as pages. They are a collection of components</p>
+);
