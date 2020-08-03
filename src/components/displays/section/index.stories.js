bfc6217272c3cf439a3e185ee969ea9435f94d4a
s3ak <nsineke@gmail.com>
2020-08-03 02:55:00 +0200

    refactor(): Update section component

--- src/components/displays/section/index.stories.js
+++ src/components/displays/section/index.stories.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { withKnobs } from "@storybook/addon-knobs";
 
 import { Section } from "./index";
 
 export default {
   title: "Displays/Section",
   component: Section,
-  decorators: [withKnobs],
 };
 
 export const Base = () => <Section>Inside the section</Section>;
